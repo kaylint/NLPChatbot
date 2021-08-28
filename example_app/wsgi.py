61b744d420c8abf24c077b5187886913f743c8b4
kaylint <kaylintwj@gmail.com>
2021-08-28 15:29:03 +0800

    remove from app

--- example_app/wsgi.py
+++ example_app/wsgi.py
@@ -3,6 +3,7 @@ import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/root/customer-account-automation/")
 
-from app import app as application
+#from app 
+import app as application
 app = application
 application.run()
