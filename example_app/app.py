7c0f7deebccb624bc99577e5e891ac6a45c49af6
kaylint <kaylintwj@gmail.com>
2021-09-07 18:12:04 +0800

    similarity threshold 0.7

--- example_app/app.py
+++ example_app/app.py
@@ -21,7 +21,7 @@ def create_app():
             {
                 "import_path": "chatterbot.logic.BestMatch",
                 'default_response': 'I am sorry, but I do not understand.',
-                'maximum_similarity_threshold': 0.50
+                'maximum_similarity_threshold': 0.70
             },
 
 
