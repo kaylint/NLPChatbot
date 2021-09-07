91df43943ab9accc589765e4dd4a3bec413a0a98
kaylint <kaylintwj@gmail.com>
2021-09-07 19:49:02 +0800

    add maths

--- example_app/app.py
+++ example_app/app.py
@@ -20,11 +20,10 @@ def create_app():
         logic_adapters=[
             {
                 "import_path": "chatterbot.logic.BestMatch",
-                'default_response': 'I am sorry, but I do not understand.',
+                'default_response': 'I am sorry, but I do not understand. To find out more, please call our friendly hotline at +65 1234 5678, open daily from 8am to 8pm.',
                 'maximum_similarity_threshold': 0.7
             },
-
-
+            'chatterbot.logic.MathematicalEvaluation',
             'chatterbot.logic.TimeLogicAdapter'
         ],
         database_uri='sqlite:///database.sqlite3'
