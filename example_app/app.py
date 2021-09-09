a6eddedf25cf08496b1625842c498bbcfae675ab
kaylint <kaylintwj@gmail.com>
2021-09-09 21:34:13 +0800

    random response

--- example_app/app.py
+++ example_app/app.py
@@ -1,9 +1,11 @@
 # imports
-from chatterbot import ChatBot
 from flask import Flask, render_template, request, jsonify
+from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.response_selection import get_random_response
 import json
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,18 +15,20 @@ def create_app():
         "Bob",
         preprocessors=['chatterbot.preprocessors.clean_whitespace'])
 
+        
+
     # set up sql database
     bot = ChatBot(
         'Bob',
+        response_selection_method=get_random_response,
         storage_adapter='chatterbot.storage.SQLStorageAdapter',
         logic_adapters=[
             {
                 "import_path": "chatterbot.logic.BestMatch",
                 'default_response': 'I am sorry, but I do not understand. To find out more, please call our friendly hotline at +65 1234 5678, open daily from 8am to 8pm.',
-                'maximum_similarity_threshold': 0.7,
-                "response_selection_method": "chatterbot.response_selection.get_random_response"
+                'maximum_similarity_threshold': 0.7
             },
-            'chatterbot.logic.MathematicalEvaluation',
+            'chatterbot.logic.MathematicalEvaluation'
             #'chatterbot.logic.TimeLogicAdapter'
         ],
         database_uri='sqlite:///database.sqlite3'
@@ -33,11 +37,11 @@ def create_app():
     # train bot - custom corpus
     trainer = ChatterBotCorpusTrainer(bot)
     trainer.train(
-        "./data/conversation.yml",
-        "./data/faq.yaml",
-        "./data/greetings.yaml",
-        "./data/orders.yaml",
-        "./data/dhlexpress.yaml"
+        "../data/conversation.yml",
+        "../data/faq.yaml",
+        "../data/greetings.yaml",
+        "../data/orders.yaml",
+        "../data/dhlexpress.yaml"
     )
 
     # #train bot - english corpus
